fa520167f3f9a7179e1dd86622e858f9e997e78d
sharon-tieu <tieusharon@gmail.com>
2022-08-29 16:30:44 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,15 @@
+var fullName = 'Sharon Tieu';
+var isCool = true;
+var totalPets = 1;
+var unknown = null;
+var never;
+
+console.log('fullName: ' + fullName);
+console.log('isCool: ' + isCool);
+console.log('totalPets: ' + totalPets);
+console.log('typeof fullName: ' + typeof fullName);
+console.log('typeof isCool: ' + typeof isCool);
+console.log('value of unknown: ' + unknown);
+console.log('typeof unknown: ' + typeof unknown);
+console.log('value of never: ' + never);
+console.log('typeof never: ' + typeof never);
