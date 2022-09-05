81409c45ef2b3a2d401f4984cfcd6f33b8d03f81
sharon-tieu <tieusharon@gmail.com>
2022-09-05 02:50:20 +0000

    Completed array-manipulation-2 exercise.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,32 @@
 /* exported chunk */
+// write a function that will take the number of a given count of the original array and set it to a new array as a subarray and then returned as a new array all together
+// return value is a parent array with child arrays
+//    assign a new variable to an empty array for the parent array
+//    assign a new variable to an empty array for the child array
+// special case:
+//    if the length of the array is less than the size, then return the array
+// use a for loop to iterate through the entire loop
+//    initialization: begin at the start of the array
+//    condition: iterate to the end of the array
+//    final expression: increment
+// if the length of the child array reaches to be equal to the same number as size, then push the child array into the parent array
+// reset the child array to make another
+// keep pushing elements into the child array to make children arrays
+// push the child array to parent array if there is a remainder element
+// return the parent array
+function chunk(array, size) {
+  var parentArray = [];
+  var childArray = [];
+  if (array.length < size) {
+    return array;
+  }
+  for (var i = 0; i < array.length; i++) {
+    if (childArray.length === size) {
+      parentArray.push(childArray);
+      childArray = [];
+    }
+    childArray.push(array[i]);
+  }
+  parentArray.push(childArray);
+  return parentArray;
+}
--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -4,11 +4,18 @@
 // special case:
 //    if the count is larger than the length of the array, then return the original array
 // loop through the array with a for loop
-//    initialization: end of the array
-//    condition: iterate through the array until it reaches the beginning so keep iterating when i is still greater than or equal to 0
-//    final expression: decrement
-// if i is less than count, then put element in new array
-//    use .unshift() so then the most recent element will be placed in the beginning of the array
-// break out of the loop
-// else if ...
-// if
+//    initialization: begin at the start of the array
+//    condition: the loop should continue to iterate as long as i is less than the difference between the length of the array and count
+//    final expression: increment
+// push the element of the array into the new array
+// return array
+function dropRight(array, count) {
+  var newArray = [];
+  if (array.length < count) {
+    return array;
+  }
+  for (var i = 0; i < array.length - count; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
