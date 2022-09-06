c0ab063b85d40772cfe5d86388a9eb4916baa02e
sharon-tieu <tieusharon@gmail.com>
2022-09-06 16:12:34 +0000

    In progress.

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,13 +1,12 @@
 /* exported pick */
-// write a function that will return an object that will contain the given source along with its corresponding key.
-// the return value is an object so assign a new variable to an empty object.
-// loop through the object with a for loop
-// function pick(source, key) {
-//   var newObject = {};
-//   for (var i = 0; i < key.length; i++) {
-//     if (source.hasOwnProperty(keys[i])) {
-//       newObject[key[i]] = source[key[i]];
-//     }
-//   }
-//   return newObject;
-// }
+function pick(source, keys) {
+  var newObject = {};
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    var value = source[key];
+    if (value !== undefined) {
+      newObject[key] = value;
+    }
+  }
+  return newObject;
+}
