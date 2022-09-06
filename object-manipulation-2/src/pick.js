01987f0516acb653de5f16c0bb9dfa70d03adf9b
sharon-tieu <tieusharon@gmail.com>
2022-09-06 07:45:23 +0000

    In progress.

---
+++ object-manipulation-2/src/pick.js
@@ -0,0 +1,4 @@
+/* exported pick */
+// write a function that will return an object that will contain the given source along with its corresponding key.
+// the return value is an object so assign a new variable to an empty object.
+// loop through the object with a for loop
