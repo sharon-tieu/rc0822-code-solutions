190c8be882da5a062a4ba9e9a4467149572d7ed1
sharon-tieu <tieusharon@gmail.com>
2022-09-07 17:37:16 +0000

    Update: fix values of email and message values in form-values.

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -3,8 +3,8 @@ contactForm.addEventListener('submit', function (e) {
   e.preventDefault();
   var userData = {
     name: contactForm.elements.name.value,
-    email: contactForm.elements.name.value,
-    message: contactForm.elements.name.value
+    email: contactForm.elements.email.value,
+    message: contactForm.elements.message.value
   };
   console.log(userData);
 });
