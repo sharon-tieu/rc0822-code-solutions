c19acf31f995784105ba43449c732d2566e412a4
sharon-tieu <tieusharon@gmail.com>
2022-09-08 04:31:14 +0000

    In progress.

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,12 +1,15 @@
 /* exported omit */
-// write a function that will create an object that will have the properties of source that is NOT listed in keys.
-// assign an empty object to a variable
-// loop through the keys with a for loop
-//   initialization: begin at the start of keys
-//   condition: iterate until the end of the keys array is reached
-//   final expression: increment
-// determine if keys[i] does NOT match with the properties found in source
+// write a function that will return the properties of source that is NOT included in the keys array.
+// assign source to a variable
+//
+function omit(source, keys) {
+  var object = {};
+  // console.log('object:', object);
+  // console.log('keyS:', keys);
+  // for (var key in object) {
+  //   if (!keys.includes(key)) {
 
-// set a new variable to keys[i]
-// if it DOES NOT match with properties in source, then push the property and value of that into the new object
-// return the new object
+  //   }
+  // }
+  return object;
+}
