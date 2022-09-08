12bce04e991e71d76d3fb1a30d4de155e2517be9
sharon-tieu <tieusharon@gmail.com>
2022-09-08 23:42:51 +0000

    invert.js in progress.

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,18 @@
 /* exported invert */
+// write a function that will reassign the original properties to the value of its value and vice versa of the source object
+// the return value is an object so assign a variable to an empty object
+//
+// function invert(source) {
+//   var object = {};
+//   for (var key in source) {
+//     var newValue = source[key];
+//     // console.log('key:', key);
+//     newValue = key;
+//     var newKey = source[key];
+//     // console.log('newValue:', newValue);
+//     // console.log('newKey:', newKey);
+//     // console.log('newValue:', newValue);
+//     // console.log('newKey:', newKey);
+//   }
+//   // return object[newKey] = newValue;
+// }
