4a3c658626e04dd0834cf65da8aa372e3d22da3f
sharon-tieu <tieusharon@gmail.com>
2022-09-09 18:17:36 +0000

    In progress.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,4 @@
 /* exported defaults */
+// write a function that will return the key/value pairs of the target object along with the key/value pairs that is also found in source object.
+// the return value will be the target object. assign to variable
+//
