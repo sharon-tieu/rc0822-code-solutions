0b98e8a3e948f1892ab5e732d42b2a92600863fe
sharon-tieu <tieusharon@gmail.com>
2022-09-23 04:19:54 +0000

    added circle carousel function. Must fix count in rightArrow function.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,35 +1,39 @@
-var $circle0 = document.querySelector('.circle-image-0');
-var $circle1 = document.querySelector('.circle-image-1');
-var $circle2 = document.querySelector('.circle-image-2');
-var $circle3 = document.querySelector('.circle-image-3');
-var $circle4 = document.querySelector('.circle-image-4');
-
+var $circle0 = document.querySelector('#circle-image-0');
+var $circle1 = document.querySelector('#circle-image-1');
+var $circle2 = document.querySelector('#circle-image-2');
+var $circle3 = document.querySelector('#circle-image-3');
+var $circle4 = document.querySelector('#circle-image-4');
 var $leftArrow = document.querySelector('.left-arrow');
 var $rightArrow = document.querySelector('.right-arrow');
 
 var $pokemonImage = document.querySelector('.pokemon-image-1');
 
 var $images = ['./images/001.png', './images/004.png', './images/007.png', './images/025.png', './images/039.png'];
-// var circleArray = [$circle0, $circle1, $circle2, $circle3, $circle4];
 var count = 0;
 
-// var dotRotation = setInterval(dotCarousel, 2000);
-// function dotCarousel() {
-//   var currentDot = circleArray[count];
-//   if (count === circleArray.length - 1) {
-//     currentDot.className.remove('fa-regular');
-//     currentDot.className.add('fa-solid');
-//   }
-// }
+var $allCircles = document.querySelectorAll('.fa-circle');
+// var carousel = setInterval(function () {
+function carousel() {
+  for (var i = 0; i < $allCircles.length; i++) {
+    $allCircles[i].setAttribute('class', 'fa-regular fa-circle fa-1x padding-left-30');
+  }
+  var currentCircle = document.querySelector('#circle-image-' + count.toString());
+  currentCircle.classList.add('fa-solid');
+}
+// setInterval(carousel, 2000);
 
 var rightArrowClick = setInterval(rightArrow, 2000);
+// setInterval(carousel, 2000);
 $rightArrow.addEventListener('click', rightArrow);
 function rightArrow() {
+  // clearInterval(carousel);
+  console.log('COUNT:', count);
+  count++;
+  carousel();
   if (count === $images.length - 1) {
     count = 0;
     $pokemonImage.src = $images[0];
   } else {
-    count++;
     $pokemonImage.src = $images[count];
     clearInterval(rightArrowClick);
     rightArrowClick = setInterval(rightArrow, 2000);
@@ -48,6 +52,7 @@ function leftArrow() {
 }
 
 $circle0.addEventListener('click', function () {
+  // clearInterval(carousel);
   $pokemonImage.src = $images[0];
   count = 0;
   $circle0.classList.add('fa-solid');
@@ -58,6 +63,7 @@ $circle0.addEventListener('click', function () {
 });
 
 $circle1.addEventListener('click', function () {
+  // clearInterval(carousel);
   $pokemonImage.src = $images[1];
   count = 1;
   $circle0.classList.remove('fa-solid');
@@ -69,6 +75,7 @@ $circle1.addEventListener('click', function () {
 });
 
 $circle2.addEventListener('click', function () {
+  // clearInterval(carousel);
   $pokemonImage.src = $images[2];
   count = 2;
   $circle0.classList.remove('fa-solid');
@@ -81,6 +88,7 @@ $circle2.addEventListener('click', function () {
 });
 
 $circle3.addEventListener('click', function () {
+  // clearInterval(carousel);
   $pokemonImage.src = $images[3];
   count = 3;
   $circle0.classList.remove('fa-solid');
@@ -92,6 +100,7 @@ $circle3.addEventListener('click', function () {
 });
 
 $circle4.addEventListener('click', function () {
+  // clearInterval(carousel);
   $pokemonImage.src = $images[4];
   count = 4;
   $circle0.classList.remove('fa-solid');
