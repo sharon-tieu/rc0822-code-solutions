a3ab2ad099d51cda131d1fe6f2de6929859e3bc0
sharon-tieu <tieusharon@gmail.com>
2022-10-19 20:23:32 +0000

    Completed delete route. Put route in progress.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -74,30 +74,41 @@ app.post('/api/notes', (req, res) => {
 // client can DELETE a note by id
 app.delete('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id);
-  // console.log('req.params.id:', id);
-  // console.log('typeof req.params.id:', typeof id);
-  // console.log('notesData[req.params.id]:', notesData[req.params.id]);
+  const data = notesData;
+  // console.log('data.notes[req.params.id]:', data.notes[req.params.id]);
 
-  if (Number.isInteger(id) === false) {
-    res.status(400).json({ error: 'id must be a positive integer' });
-  } else if (id < 0) {
-    res.status(400).json({ error: 'id must be a positive integer' });
-  } else if (id !== notesData[id]) {
-    res.status(404).json({ error: `cannot find note with id ${id}` });
-  } else {
-    delete notesData[req.params.id];
-    res.status(204).json();
+  if (Number.isInteger(id) === false || id < 1) {
+    res.status(400).json({
+      error: 'id must be a positive integer'
+    });
+    return;
   }
+  if (typeof data.notes[id] === 'undefined') {
+    res.status(404).json({
+      error: `cannot find note with id ${id}`
+    });
+    return;
+  }
+
+  delete data.notes[req.params.id];
+
+  const json = JSON.stringify(data, null, 2);
+  fs.writeFile('data.json', json, err => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({
+        error: 'an unexpected error occurred'
+      });
+    } else {
+      res.sendStatus(204);
+    }
+  });
 });
+
+// client can PUT
 
 const port = 3000;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`listening on port ${port}~`);
 });
-
-// funtion handleErrors(req, res) {
-//   if (!notesData.notes[req.params.id] && isNaN(req.params.id)) {
-//     res.status(400).json({ })
-//   }
-// }
