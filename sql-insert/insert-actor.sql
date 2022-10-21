ec57338fc4b842661329469352e75f5e44adce31
sharon-tieu <tieusharon@gmail.com>
2022-10-21 03:56:46 +0000

    Completed sql-insert exercise.

---
+++ sql-insert/insert-actor.sql
@@ -0,0 +1,5 @@
+insert into "actors" ("firstName", "lastName")
+  values ('Sharon', 'Tieu');
+
+select *
+  from "actors";
---
+++ sql-insert/insert-languages.sql
@@ -0,0 +1,7 @@
+insert into "languages" ("name")
+values ('HTML'), ('CSS'), ('JavaScript');
+
+select *
+  from "languages";
+-- below will return the rows you want to add (Lines 1-2)
+-- returning *;
