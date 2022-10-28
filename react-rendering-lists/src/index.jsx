e1b9d5e9ee0b600aa43decc443a28e58335a855e
sharon-tieu <tieusharon@gmail.com>
2022-10-28 21:16:12 +0000

    Completed react-rendering-lists exercise.

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function ListPokemonItems(props) {
+  return <li>{ props.value }</li>;
+}
+
+function PokemonList(props) {
+  const pokedexList = props.pokedex;
+  const listItems = pokedexList.map(pokemon =>
+    <ListPokemonItems key={ pokemon.number.toString()} value={ pokemon.name } />
+  );
+
+  return (
+    <ul>
+      { listItems }
+    </ul>
+  );
+}
+
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<PokemonList pokedex={pokedex} />);
