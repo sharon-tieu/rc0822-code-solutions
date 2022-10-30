9f17907b6af323c490d43856040d80e1c2a9ba56
sharon-tieu <tieusharon@gmail.com>
2022-10-30 20:38:24 +0000

    Completed react-toggle-switch-component challenge.

---
+++ react-toggle-switch-component/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ToggleSwitch from './toggle-switch';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<ToggleSwitch />);
