15ba67f47726d8e42625ebf021bd60699dde7178
sharon-tieu <tieusharon@gmail.com>
2022-10-31 17:31:09 +0000

    Completed react-validated-input-component challenge.

---
+++ react-validated-input-component/src/validate-input.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+export default class ValidateInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: ''
+    };
+    this.CharacterCheck = this.CharacterCheck.bind(this);
+  }
+
+  CharacterCheck(event) {
+    const password = event.target.value;
+
+    this.setState({ password: event.target.value });
+    if (password.length < 8) {
+      this.setState({
+        password: event.target.value
+      });
+    } else {
+      this.setState({
+        isValid: true
+      });
+    }
+  }
+
+  render() {
+    let message = '';
+    let icon = '';
+
+    if (this.state.password.length === 0) {
+      message = 'A password is required.';
+      icon = 'fa-solid fa-xmark fa-2xl red';
+    } else if (this.state.password.length < 8) {
+      message = 'Your password is too short.';
+      icon = 'fa-solid fa-xmark fa-2xl red';
+    } else {
+      icon = 'fa-solid fa-check fa-2xl green';
+    }
+
+    return (
+      <form>
+        <label htmlFor="password">Password</label>
+        <div>
+          <input type="password" name="password" id="password" onChange={ this.CharacterCheck } />
+          <i className={`${icon}`}></i>
+        </div>
+        <p>{`${message}`}</p>
+      </form>
+    );
+  }
+}
