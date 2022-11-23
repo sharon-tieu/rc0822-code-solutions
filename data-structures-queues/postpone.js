9cb285860b26b686e8d9dcf2fa22ff3f2a5885f9
sharon-tieu <tieusharon@gmail.com>
2022-11-23 00:14:41 +0000

    Completed postpone(queue).

--- data-structures-queues/postpone.js
+++ data-structures-queues/postpone.js
@@ -1,5 +1,9 @@
 /* exported postpone */
 
 function postpone(queue) {
-
+  if (queue.peek() === undefined) {
+    return;
+  }
+  const frontValue = queue.dequeue();
+  queue.enqueue(frontValue);
 }
