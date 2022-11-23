8bc6e0cc48a1b074b317382920dabb34f4ed89bb
sharon-tieu <tieusharon@gmail.com>
2022-11-23 01:02:00 +0000

    Completed take2nd(queue).

--- data-structures-queues/take-2nd.js
+++ data-structures-queues/take-2nd.js
@@ -1,5 +1,14 @@
 /* exported take2nd */
 
 function take2nd(queue) {
-
+  if (queue.peek() === undefined) {
+    return;
+  }
+  const firstItem = queue.dequeue();
+  if (queue.peek() === undefined) {
+    return firstItem;
+  }
+  queue.enqueue(firstItem);
+  const secondItem = queue.dequeue();
+  return secondItem;
 }
