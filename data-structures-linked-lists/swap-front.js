24ef1a2b05fe5a7779cc6252d7fd726739430581
sharon-tieu <tieusharon@gmail.com>
2022-11-28 20:52:40 +0000

    Completed swapFront(list).

--- data-structures-linked-lists/swap-front.js
+++ data-structures-linked-lists/swap-front.js
@@ -1,5 +1,12 @@
 /* exported swapFront */
 
 function swapFront(list) {
-
+  if (list.next === null) {
+    return list;
+  }
+  const next = list.next;
+  const nextAfter = list.next.next;
+  next.next = list;
+  list.next = nextAfter;
+  return next;
 }
